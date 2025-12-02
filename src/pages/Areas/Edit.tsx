8898f172d3afda3b3933c818b6da6b7eabe3665b
MiguelMartini <miguelkaulingm21@gmail.com>
2025-12-02 14:35:48 -0300

    Fix: Nova navbar nos componentes

--- src/pages/Areas/Edit.tsx
+++ src/pages/Areas/Edit.tsx
@@ -1,24 +1,29 @@
-import Menu from "@/components/custom/Menu";
+import Navbar from "@/components/custom/Navbar";
 import Tabela from "@/components/custom/Tabela";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 
-
 const Edit = () => {
-
   const navigate = useNavigate();
 
   return (
     <div>
-      <div className="flex justify-center flex-col items-center min-h-screen px-4">
-        <Menu />
+      <Navbar />
+      <div className="flex justify-center flex-col items-center mt-10">
         <div className="w-full sm:w-full md:w-200 lg:w-[800px] flex flex-col justify-center bg-[#AAD3DF]/60 m-10 p-4 rounded-xl inset-shadow-sm shadow-xl/20 mt-25">
-        <p className="p-4 flex justify-start font-bold text-2xl">Editar áreas esportivas</p>
-        <div className="p-0.5 bg-gray-500 mt-4 rounded-4xl mb-4"></div>
+          <p className="p-4 flex justify-start font-bold text-2xl">
+            Editar áreas esportivas
+          </p>
+          <div className="p-0.5 bg-gray-500 mt-4 rounded-4xl mb-4"></div>
           <Tabela />
         </div>
         <div className="mt-4 mb-4">
-          <Button onClick={() => navigate('/home')} className="active:scale-[.98] py-8 px-20 md:py-6 lg:py-7 rounded-xl text-white text-lg font-bold cursor-pointer bg-amber-600 hover:bg-blue-700">Voltar</Button>
+          <Button
+            onClick={() => navigate("/home")}
+            className="active:scale-[.98] py-8 px-20 md:py-6 lg:py-7 rounded-xl text-white text-lg font-bold cursor-pointer bg-amber-600 hover:bg-blue-700"
+          >
+            Voltar
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/Areas/Update.tsx
+++ src/pages/Areas/Update.tsx
@@ -2,6 +2,7 @@ import { editArea, getArea } from "@/api/api";
 import Btn from "@/components/custom/Btn";
 import InputForm from "@/components/custom/inputForm";
 import Menu from "@/components/custom/Menu";
+import Navbar from "@/components/custom/Navbar";
 import TextForm from "@/components/custom/TextForm";
 import { Spinner } from "@/components/ui/spinner";
 import axios from "axios";
@@ -22,7 +23,6 @@ interface FormState {
 }
 
 const Update = () => {
-
   const { id } = useParams();
   const areaId = Number(id);
 
@@ -35,7 +35,7 @@ const Update = () => {
     cidade: "",
     estado: "",
     cep: "",
-    complemento: ""
+    complemento: "",
   });
 
   const navigate = useNavigate();
@@ -55,46 +55,46 @@ const Update = () => {
 
   useEffect(() => {
     const fectchArea = async () => {
-        try{
-          const response = await getArea(areaId)
-          const info = response.data.message
-          const endereco = response.data.message.endereco
-          setForm(prev => ({
-            ...prev,
-            titulo: info.titulo,
-            descricao: info.descricao,
-            rua: endereco.rua || "",
-            bairro: endereco.bairro || "",
-            cidade: endereco.cidade || "",
-            estado: endereco.estado || "",
-            cep: endereco.cep || "",
-            numero: endereco.numero || "",
-            complemento: endereco.complemento || "",
-          }))
-          toast.success("Dados Carregados com sucesso!")
-        }catch(error: any){
-          console.log(error)
-        }
+      try {
+        const response = await getArea(areaId);
+        const info = response.data.message;
+        const endereco = response.data.message.endereco;
+        setForm((prev) => ({
+          ...prev,
+          titulo: info.titulo,
+          descricao: info.descricao,
+          rua: endereco.rua || "",
+          bairro: endereco.bairro || "",
+          cidade: endereco.cidade || "",
+          estado: endereco.estado || "",
+          cep: endereco.cep || "",
+          numero: endereco.numero || "",
+          complemento: endereco.complemento || "",
+        }));
+        toast.success("Dados Carregados com sucesso!");
+      } catch (error: any) {
+        console.log(error);
+      }
     };
     fectchArea();
   }, []);
 
   const handleChange = (field: string, value: string) => {
-    setForm(prev => ({
-    ...prev,
-    [field]: value
-   }));
+    setForm((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
   };
-  const consultaCep = async (cep:string)  =>{
-    try{
+  const consultaCep = async (cep: string) => {
+    try {
       const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
       const data = response.data;
-      if(data.erro){
-        toast.error("CEP não encontrado")
+      if (data.erro) {
+        toast.error("CEP não encontrado");
         return;
       }
 
-      setForm(prev => ({
+      setForm((prev) => ({
         ...prev,
         rua: data.logradouro || "",
         bairro: data.bairro || "",
@@ -103,27 +103,25 @@ const Update = () => {
         complemento: data.complemento || "",
       }));
 
-      toast.success("Endereço atualizado!")
-    }catch(error){
-      toast.error("Erro ao buscar CEP")
+      toast.success("Endereço atualizado!");
+    } catch (error) {
+      toast.error("Erro ao buscar CEP");
     }
-  }
+  };
 
   const handleUpdate = async () => {
     setLoading(true);
     setErrors({});
     try {
-
       const payload = {
-      ...form,
-      numero: form.numero === "" ? null : Number(form.numero)
-    };
+        ...form,
+        numero: form.numero === "" ? null : Number(form.numero),
+      };
       const response = await editArea(payload, id);
       toast.success(`${response.data.message}`);
       navigate("/home");
-
     } catch (error: any) {
-      console.log(error)
+      console.log(error);
       if (error.response && error.response.data.message) {
         const msgs = error.response.data.message;
         const backendErrors: any = {};
@@ -145,18 +143,18 @@ const Update = () => {
   };
   return (
     <div>
-      <div className="flex justify-center flex-col items-center min-h-screen">
-        <Menu />
-        <div
-          className="flex flex-col justify-center my-20 bg-[#AAD3DF]/60 p-12 rounded-xl inset-shadow-sm shadow-xl/20"
-        >
+      <Navbar />
+      <div className="flex justify-center flex-col items-center bg">
+        <div className="flex flex-col justify-center mt-10 bg-[#AAD3DF]/60 p-12 rounded-xl inset-shadow-sm shadow-xl/20">
           <p className="text-3xl font-bold text-gray-800">
             Editar área esportiva : Id #{id}
           </p>
           <div className="p-0.5 bg-gray-500 mt-3 rounded-4xl"></div>
           <div className="lg:flex flex-row justify-center sm:flex flex-col">
             <div>
-              <p className="text-xl font-semibold text-gray-700 mt-5">Informações</p>
+              <p className="text-xl font-semibold text-gray-700 mt-5">
+                Informações
+              </p>
               {/* <div className="mt-5 ">
                 <div className="bg-gray-50 rounded-xl">
                 <Input type="file"/>
@@ -183,9 +181,11 @@ const Update = () => {
             </div>
             <div className="mx-6"></div>
             {/* coluna dos endereços */}
-            
-            <div className= "lg:w-1/2 sm: w-full">
-              <p className="text-xl font-semibold text-gray-700 mt-5">Endereço</p>
+
+            <div className="lg:w-1/2 sm: w-full">
+              <p className="text-xl font-semibold text-gray-700 mt-5">
+                Endereço
+              </p>
               <div className="mt-4">
                 <InputForm
                   labelValue={"CEP"}
@@ -197,25 +197,25 @@ const Update = () => {
                 />
               </div>
               <div className="grid grid-cols-2 gap-4">
-              <div className="mt-4">
-                <InputForm
-                  labelValue={"Rua"}
-                  placeholder={"Digite sua rua"}
-                  value={form.rua}
-                  onChange={(v) => handleChange("rua", v)}
-                  error={errors.rua}
+                <div className="mt-4">
+                  <InputForm
+                    labelValue={"Rua"}
+                    placeholder={"Digite sua rua"}
+                    value={form.rua}
+                    onChange={(v) => handleChange("rua", v)}
+                    error={errors.rua}
                   />
-              </div>
-              <div className="mt-4">
-                <InputForm
-                  labelValue={"Bairro"}
-                  placeholder={"Digite seu bairro"}
-                  value={form.bairro}
-                  onChange={(v) => handleChange("bairro", v)}
-                  error={errors.bairro}
+                </div>
+                <div className="mt-4">
+                  <InputForm
+                    labelValue={"Bairro"}
+                    placeholder={"Digite seu bairro"}
+                    value={form.bairro}
+                    onChange={(v) => handleChange("bairro", v)}
+                    error={errors.bairro}
                   />
+                </div>
               </div>
-                  </div>
               <div className="mt-4">
                 <InputForm
                   labelValue={"Cidade"}
@@ -226,26 +226,26 @@ const Update = () => {
                 />
               </div>
               <div className="grid grid-cols-2 gap-4 mt-4">
-              <div className="mt-4">
-                <InputForm
-                  labelValue={"Estado"}
-                  placeholder={"Digite o estado"}
-                  value={form.estado}
-                  onChange={(v) => handleChange("estado", v)}
-                  error={errors.estado}
+                <div className="mt-4">
+                  <InputForm
+                    labelValue={"Estado"}
+                    placeholder={"Digite o estado"}
+                    value={form.estado}
+                    onChange={(v) => handleChange("estado", v)}
+                    error={errors.estado}
                   />
-              </div>
-              <div className="mt-4">
-                <InputForm
-                  labelValue={"Número"}
-                  placeholder={"Digite o nº da área"}
-                  value={form.numero}
-                  error={errors.numero}
-                  type="number"
-                  onChange={(v) => handleChange("numero", v)}
+                </div>
+                <div className="mt-4">
+                  <InputForm
+                    labelValue={"Número"}
+                    placeholder={"Digite o nº da área"}
+                    value={form.numero}
+                    error={errors.numero}
+                    type="number"
+                    onChange={(v) => handleChange("numero", v)}
                   />
+                </div>
               </div>
-                  </div>
               <div className="mt-4">
                 <InputForm
                   labelValue={"Complemento"}
@@ -258,9 +258,12 @@ const Update = () => {
             </div>
           </div>
           <div className="flex flex-col items-stretch sm:flex-col lg:flex-row justify-center mt-10 gap-3">
-            <Btn value="Voltar" onClick={() =>navigate("/areas/edit")}/>
-            <Btn value="Cancelar" onClick={() =>navigate("/home")}/>
-            <Btn value={loading ? <Spinner /> : "Salvar"} onClick={() => (handleUpdate())}/>
+            <Btn value="Voltar" onClick={() => navigate("/areas/edit")} />
+            <Btn value="Cancelar" onClick={() => navigate("/home")} />
+            <Btn
+              value={loading ? <Spinner /> : "Salvar"}
+              onClick={() => handleUpdate()}
+            />
           </div>
         </div>
       </div>
