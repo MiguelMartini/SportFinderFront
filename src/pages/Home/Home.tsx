16af6d4000ca7f2e42c97efded3b3713c3be271d
MiguelMartini <miguelkaulingm21@gmail.com>
2025-12-01 22:24:01 -0300

    Feat: Nova Navbar do sistema

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 
 import MapComponent from "@/components/custom/Map";
 import Menu from "@/components/custom/Menu";
+import Navbar from "@/components/custom/Navbar";
 
 const Home = () => {
   return (
      <div>
-      <Menu/>
+      {/* <Menu/> */}
+      <Navbar/>
       <MapComponent/>
     </div>
   )
